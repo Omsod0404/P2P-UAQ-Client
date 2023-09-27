d4837a0d795ee76fc2904ab07cdd45e1a93ef83d
MSIgamer <ramnoah2308@gmail.com>
2023-09-27 16:09:43 -0600

    client views

---
+++ Views/Client_chat_private.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P2P_UAQ_Client.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Client_chat_private.xaml
+    /// </summary>
+    public partial class Client_chat_private : Window
+    {
+        public Client_chat_private()
+        {
+            InitializeComponent();
+        }
+    }
+}
